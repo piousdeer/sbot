aefe874d8a067a88efef7aaf17a740213b61dc8e
Rult <20477460+Rult@users.noreply.github.com>
2019-06-10 18:12:38 +0300

    Change .includes() to .startsWith() in autovote

--- bot.js
+++ bot.js
@@ -64,7 +64,7 @@ function processMessage(msg) {
 		msgCommandOriginal = msgoc
 		msgCommand = msglc
 	} else {
-		if (msg.guild.id == "540145900526501899" && msgoc.includes("<:vote:587648714726965258>")) {
+		if (msgoc.startsWith("<:vote:587648714726965258>")) {
 			msg.react(s.findEmoji("540543141649055769"))
 				.then(() => {
 					msg.react(s.findEmoji("540543158560358421"))
