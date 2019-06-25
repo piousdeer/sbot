a78b84f29e2d73c1bbfc0e8d4b98ddf3e6bc955a
Rult <20477460+Rult@users.noreply.github.com>
2019-06-25 11:32:59 +0300

    Do not use setCinemaRole

--- bot.js
+++ bot.js
@@ -123,9 +123,10 @@ function actionsForReactions(messageReaction, user, wasReactionAdded) {
 	let msg = messageReaction.message
 	let msgReaction = messageReaction.emoji.name
 
-	if (["📽", "⛩", "🎮"].includes(msgReaction) && msg.id == "565292786514133012") {
-		s.setCinemaRole(user, wasReactionAdded, msgReaction)
-	} else if (msg.content.startsWith("Доступные эмоджи:") && ["⬅", "➡"].includes(msgReaction)) {
+//	if (["📽", "⛩", "🎮"].includes(msgReaction) && msg.id == "565292786514133012") {
+//		s.setCinemaRole(user, wasReactionAdded, msgReaction)
+//	} else 
+	if (msg.content.startsWith("Доступные эмоджи:") && ["⬅", "➡"].includes(msgReaction)) {
 		s.checkEmojiListReaction(msgReaction, user, msg, visibleServers)
 	} else {
 		s.checkHomestuckReaction(messageReaction, user)
