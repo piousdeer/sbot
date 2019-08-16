514c20a5713026f6a4cb745cc70b2798a2abd8d5
Rult <20477460+Rult@users.noreply.github.com>
2019-08-16 09:34:52 +0300

    Exception for entirely transparent images

--- secondary.js
+++ secondary.js
@@ -298,17 +298,22 @@ export async function getMainColorFromImage(link, callback) {
 					}
 
 					if (x == image.bitmap.width - 1 && y == image.bitmap.height - 1) {
-						let centroids = skmeans(dataset, 10, "kmpp", 100).centroids
-						let hsvColors = []
-						for (let i = 0; i < centroids.length; i++) {
-							hsvColors.push(rgb2hsv(centroids[i]))
+						let color
+						if (dataset.length) {
+							let centroids = skmeans(dataset, 10, "kmpp", 100).centroids
+							let hsvColors = []
+							for (let i = 0; i < centroids.length; i++) {
+								hsvColors.push(rgb2hsv(centroids[i]))
+							}
+							hsvColors.sort((a, b) => {
+								return (b[1]+0.01)*b[2]*b[2] - (a[1]+0.01)*a[2]*a[2]
+							})
+	
+							let colorRGB = hsv2rgb(hsvColors[0])
+							color = colorRGB[0]*256*256 + colorRGB[1]*256 + colorRGB[2]
+						} else {
+							color = undefined
 						}
-						hsvColors.sort((a, b) => {
-							return (b[1]+0.01)*b[2]*b[2] - (a[1]+0.01)*a[2]*a[2]
-						})
-
-						let colorRGB = hsv2rgb(hsvColors[0])
-						let color = colorRGB[0]*256*256 + colorRGB[1]*256 + colorRGB[2]
 
 						if (callback) {
 							callback(color)
