57194b08b81c274f8bcd28c4a9e5ef614d2ae14f
user <user@example.com>
2019-12-25 02:57:12 +0300

    Fix some kana data

--- japdata.js
+++ japdata.js
@@ -29,8 +29,8 @@ export const katakana = {
 export const kanalat = [
     [ "a",  "i",  "u",  "e",  "o"],
     ["ka", "ki", "ku", "ke", "ko"], ["ga", "gi", "gu", "ge", "go"],
-    ["sa","shi", "su", "se", "so"], ["za", "zi", "zu", "ze", "zo"],
-    ["ta","chi","tsu", "te", "to"], ["da", "di", "du", "de", "do"],
+    ["sa","shi", "su", "se", "so"], ["za", "ji", "zu", "ze", "zo"],
+    ["ta","chi","tsu", "te", "to"], ["da", "ji", "zu", "de", "do"],
     ["na", "ni", "nu", "ne", "no"],
     ["ha", "hi", "fu", "he", "ho"], ["ba", "bi", "bu", "be", "bo"], ["pa", "pi", "pu", "pe", "po"],
     ["ma", "mi", "mu", "me", "mo"],
