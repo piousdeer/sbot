7f317c4c9788f3389900e0904739d9f68b63fefb
Rult <20477460+Rult@users.noreply.github.com>
2019-12-29 00:01:35 +0300

    more testing

--- commands.js
+++ commands.js
@@ -75,7 +75,7 @@ export const commands = {
 		v: true,
 		f (msg) {
 			let pongText = "🏓 Понг!"
-			console.log("ping test")
+			console.log("ping test more")
 			msg.channel.send(pongText)
 				.then((pong) => {
 					let userTime = msg.createdTimestamp / 1000
