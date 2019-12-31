14d720f91d466565634b627c88b0adbe9ad30490
Rult <20477460+Rult@users.noreply.github.com>
2019-12-31 23:37:52 +0300

    Remove hashtags from To Learn note

--- commands.js
+++ commands.js
@@ -1409,7 +1409,7 @@ export const commands = {
 						if (rounds) {
 							gameoverEmbed.description = `Result: ${(score/rounds*100).toFixed(2)}%`
 							if (wrongSet.size) {
-								gameoverEmbed.description += ` \nTo learn: \n${Array.from(wrongSet).map(x=>'['+x+']'+'(https://jisho.org/search/%23kanji%20'+x+')').join("　")}`
+								gameoverEmbed.description += ` \nTo learn: \n${Array.from(wrongSet).map(x=>'['+x+']'+'(https://jisho.org/search/'+x+')').join("　")}`
 							}
 						}
 
