a3c7533cbbb94135e2c018f6eebfd8f48fa30797
piousdeer <31318219+piousdeer@users.noreply.github.com>
2022-01-26 15:52:15 +0700

    Don't replace cdn.discordapp.com with cdn.discord.com

--- monkey-patch.sh
+++ monkey-patch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 echo Patching discord.js...
-find node_modules/discord.js/ -type f -exec sed -i 's/discordapp\.com/discord\.com/g' {} +
+find node_modules/discord.js/ -type f -exec sed -i 's/(?<!cdn\.)discordapp\.com/discord\.com/g' {} +
 sed -i '80s/.*/if (channel != null)/' node_modules/discord.js/src/client/ClientDataManager.js
